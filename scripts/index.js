dcd12c7a57c3fa52e65655bc0261f89531f86cd6
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-07 09:05:06 +0200

    feat: implement random number generation and DOM click handling for each cell
    feat: implementa numeri casuali e click su ogni cella

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,44 @@
 
-const tabello = document.querySelector('#tabello');
-for(let i=0 ; i<100 ; i++){
-    tabello.innerHTML += ` <div class=" border border-dark " value="${i+1}"></div>`;
-    
+for (let i = 0; i < 100; i++) {
+    tabello.innerHTML += ` <div class=" border border-dark "
+    value="${i + 1}"><button class="button-table"></button></div>`;
 }
+
+
+
+
+const randomNumero = [];
+
+for (let j = 0; j < 16; j++) {
+    randomNumero.push(Math.floor(Math.random() * 100) + 1);
+}
+console.log(randomNumero);
+let count = 0;
+const cells = document.querySelectorAll('.button-table');
+cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+
+
+        const cellValue = Number(cell.parentElement.getAttribute('value'));
+        if (randomNumero.includes(cellValue)) {
+            cell.classList.add('color-red');
+            console.log('Finish game');
+            document.querySelector('#base').innerHTML = `<div class="finish text-center pt-5">
+        <h1>GAME OVER <br> Score :${count}</h1>
+        </div>`
+        } else {
+            if (count < 84) {
+                cell.classList.add('color-blu');
+                console.log('ok');
+                count++;
+            }
+            else {
+                document.querySelector('#base').innerHTML = `<div class="win text-center pt-5">
+        <h1>YOU WIN <br> Score :${count}</h1>
+        </div>`;
+            }
+
+        }
+        console.log(`Hai cliccato sulla cella ${cellValue}`);
+    });
+}); 
